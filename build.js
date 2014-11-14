417317b6c99fd60a3715db42a21dc3530672a5d7
David Moody <david@davidxmoody.com>
2014-11-14 10:54:56 +0000

    In-progress rewrite, added templates

--- build.js
+++ build.js
@@ -2,13 +2,33 @@ var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
 var permalinks = require('metalsmith-permalinks');
+var collections = require('metalsmith-collections');
 
 Metalsmith(__dirname)
+  .clean(true)
   .metadata({
     title: "David Moody's Blog",
     description: "A blog about programming"
   })
-  .clean(true)
+
+  .use(collections({
+    posts: 'posts/*.md',
+    pages: 'pages/*.md'
+  }))
+  .use(function(files, metalsmith, done) {
+    // Set default templates
+    metalsmith.metadata().posts.forEach(function(post) {
+      post.template = 'post.html';
+    });
+    metalsmith.metadata().pages.forEach(function(page) {
+      page.template = 'default.html';
+    });
+    Object.keys(files).forEach(function(file) {
+      console.log(files[file].template, file);
+    });
+    done();
+  })
+
   .use(markdown())
   .use(permalinks({
     pattern: ':title'
