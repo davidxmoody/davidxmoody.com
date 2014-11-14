664b726c759dc9f4178ac889d7cdf8b46d6d7163
David Moody <david@davidxmoody.com>
2014-11-14 12:29:20 +0000

    In-progress rewrite, fixed templates

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
 var permalinks = require('metalsmith-permalinks');
 var collections = require('metalsmith-collections');
+var each = require('metalsmith-each');
 
 Metalsmith(__dirname)
   .clean(true)
@@ -15,25 +16,25 @@ Metalsmith(__dirname)
     posts: 'posts/*.md',
     pages: 'pages/*.md'
   }))
-  .use(function(files, metalsmith, done) {
+  .use(function(files, metalsmith) {
     // Set default templates
     metalsmith.metadata().posts.forEach(function(post) {
       post.template = 'post.html';
     });
-    metalsmith.metadata().pages.forEach(function(page) {
-      page.template = 'default.html';
-    });
-    Object.keys(files).forEach(function(file) {
-      console.log(files[file].template, file);
-    });
-    done();
   })
 
   .use(markdown())
   .use(permalinks({
     pattern: ':title'
   }))
+
+  // Use templates once then once again to wrap *every file* in default.html
   .use(templates('mustache'))
+  .use(each(function(file) {
+    file.template = 'default.html';
+  }))
+  .use(templates('mustache'))
+
   .build(function(err) {
     if (err) throw err;
     console.log("Finished");
