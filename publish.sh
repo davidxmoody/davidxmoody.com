0eef33bd7cc5ef4d897575f297daf7cf346fd835
David Moody <david@davidxmoody.com>
2014-11-22 09:55:25 +0000

    Edited branch names in publish script

--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ set -e
 # Check for uncommitted changes or untracked files
 [ -n "$(git status --porcelain)" ] && git status && exit 1
 
-git branch -D master
-git checkout -b master
+git branch -D gh-pages
+git checkout -b gh-pages
 git filter-branch --subdirectory-filter build/ -f
-git checkout source
+git checkout master
 git push --all origin
