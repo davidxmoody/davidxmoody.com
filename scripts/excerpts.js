7c88f8cf9160ae432fccd2c277988d34b76784e0
David Moody <david@davidxmoody.com>
2015-07-22 20:29:58 +0100

    Converted excerpts.coffee to ES6

---
+++ scripts/excerpts.js
@@ -0,0 +1,28 @@
+const EXCERPT_SEPARATOR = /<!--\s*more\s*-->/;
+
+function paraCount(text) {
+  return text ? text.match(/<(p|ul|ol|pre|table)>[\s\S]*?<\/\1>/g).length : 0;
+}
+
+export default function() {
+  return function(files, metalsmith) {
+    for (const file of metalsmith.metadata().posts) {
+      const [before, after] = file.contents.toString().split(EXCERPT_SEPARATOR, 2);
+
+      const remainingCount = paraCount(after);
+      let readMoreText;
+      switch (remainingCount) {
+        case 0:
+          readMoreText = 'View post on separate page';
+          break;
+        case 1:
+          readMoreText = 'Read 1 remaining paragraph...';
+          break;
+        default:
+          readMoreText = `Read ${remainingCount} remaining paragraphs...`;
+      }
+
+      file.excerpt = `${before} <p><a href="${file.path}">${readMoreText}</a></p>`;
+    }
+  };
+}
