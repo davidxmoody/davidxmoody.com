ec3f36c1eb7ce8c78fd72af12a86dc474a340445
David Moody <david@davidxmoody.com>
2015-10-04 13:03:34 +0100

    Add script to print commit dates from blog repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp'
 import browserSync from 'browser-sync'
 import build from './scripts/build'
+import gitlog from 'gitlog'
 
 const paths = {
   src: 'src/**/*',
@@ -32,4 +33,29 @@ gulp.task('dev-build', cb => {
 gulp.task('watch', ['dev-build'], cb => {
   gulp.watch(paths.src, ['dev-build'])
   gulp.watch(paths.layouts, ['dev-build'])
+})
+
+const options = {
+  repo: __dirname,
+  number: 1000000,
+  author: 'David Moody',
+  fields: [
+    'authorName',
+    'subject',
+    'authorDate',
+  ],
+}
+
+function makeDate({authorDate}) {
+  // Fixes bug where merge commits have "@end@" on the end and they don't parse
+  return new Date(authorDate.replace(/@end@$/, ''))
+}
+
+gulp.task('gitlog', cb => {
+  gitlog(options, (err, commits) => {
+    if (err) return cb(err)
+    const dates = commits.map(makeDate)
+    console.log(dates)
+    cb()
+  })
 })
