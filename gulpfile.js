5d0c0917e8b510df9fc3cba3f424bbb0b8f523ac
David Moody <david@davidxmoody.com>
2015-10-04 14:22:15 +0100

    Print a list of commits grouped and sorted by day

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ import gitlog from 'gitlog'
 import glob from 'glob'
 import path from 'path'
 
+const gitAuthorName = 'David Moody'
+
 const paths = {
   src: 'src/**/*',
   layouts: 'layouts/**/*',
@@ -37,30 +39,10 @@ gulp.task('watch', ['dev-build'], cb => {
   gulp.watch(paths.layouts, ['dev-build'])
 })
 
-const options = {
-  repo: __dirname,
-  number: 1000000,
-  author: 'David Moody',
-  fields: [
-    'authorName',
-    'subject',
-    'authorDate',
-  ],
-}
-
 function makeDate({authorDate}) {
   // Fixes bug where merge commits have "@end@" on the end and they don't parse
   return new Date(authorDate.replace(/@end@$/, ''))
 }
-
-gulp.task('gitlog', cb => {
-  gitlog(options, (err, commits) => {
-    if (err) return cb(err)
-    const dates = commits.map(makeDate)
-    console.log(dates)
-    cb()
-  })
-})
 
 function discoverRepos(searchPaths) {
   return searchPaths.map(
@@ -72,13 +54,86 @@ function discoverRepos(searchPaths) {
     }
   ).reduce((a, b) => a.concat(b), []).map(
     fullPath => ({
-      path: fullPath,
+      fullPath,
       name: fullPath.replace(/^.*\//, ''),
     })
   )
 }
 
-gulp.task('glob', cb => {
-  console.log(discoverRepos(['p', 'sync/old-projects']))
-  cb()
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      repo: repo.fullPath,
+      number: 1000000,
+      author: gitAuthorName,
+      fields: [
+        'authorName',
+        'subject',
+        'authorDate',
+      ],
+    }
+
+    gitlog(options, (err, commits) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(commits.map(commit => ({
+          repoName: repo.name,
+          date: makeDate(commit),
+          subject: commit.subject,
+        })))
+      }
+    })
+  })
+}
+
+function getAllCommits(searchPaths) {
+  return Promise.all(
+    discoverRepos(searchPaths).map(getCommits)
+  ).then(
+    commitLists => commitLists.reduce((a, b) => a.concat(b), [])
+  )
+}
+
+function toYYYYMMDD(date) {
+  return date.toISOString().slice(0, 10)
+}
+
+function groupCommitsByDay(commits) {
+  const days = {}
+
+  for (const commit of commits) {
+    const day = toYYYYMMDD(commit.date)
+    if (!days[day]) days[day] = []
+    days[day].push(commit)
+  }
+
+  const sortedKeys = Object.keys(days).sort()
+  const firstDay = sortedKeys[0]
+  const lastDay = sortedKeys[sortedKeys.length - 1]
+
+  for (let d = new Date(firstDay); d < new Date(lastDay); d.setDate(d.getDate() + 1)) {
+    const dayInRange = toYYYYMMDD(d)
+    if (!days[dayInRange]) {
+      days[dayInRange] = []
+    }
+  }
+
+  // For convenience and readability, sort the object by date
+  const sortedDays = {}
+  for (const day of Object.keys(days).sort()) {
+    sortedDays[day] = days[day]
+  }
+
+  return sortedDays
+}
+
+gulp.task('gitlog', cb => {
+  getAllCommits(['p', 'sync/old-projects']).then(commits => {
+    const days = groupCommitsByDay(commits)
+    console.log(days)
+    cb()
+  }).catch(
+    err => cb(err)
+  )
 })
