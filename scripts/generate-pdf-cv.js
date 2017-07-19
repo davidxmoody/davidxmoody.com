78ac8e83b093520066364418d1e520ad2b02ea1a
David Moody <david@davidxmoody.com>
2017-07-19 11:28:11 +0100

    Try out converting from markdown instead of html
    Not sure if this is worth keeping yet...

--- scripts/generate-pdf-cv.js
+++ scripts/generate-pdf-cv.js
@@ -1,26 +1,37 @@
-const pdf = require("html-pdf")
-const fs = require("fs")
+// const pdf = require("html-pdf")
+// const httpServer = require("http-server")
+// const fs = require("fs")
 const path = require("path")
-const httpServer = require("http-server")
 
-const pathPrefix = path.join(__dirname, "../build")
-const source = path.join(pathPrefix, "cv/index.html")
-const target = path.join(pathPrefix, "cv/david-moody-cv.pdf")
+const pathPrefix = path.join(__dirname, "..")
+const source = path.join(pathPrefix, "src/cv.md")
+const target = path.join(pathPrefix, "build/cv/david-moody-cv.pdf")
+const cssPath = path.join(pathPrefix, "src/css/pdf.css")
 
-const PORT = 3033
+// const PORT = 3033
 
-const options = {
-  base: `http://localhost:${PORT}`,
-}
+// const options = {
+//   base: `http://localhost:${PORT}`,
+//   format: "A4",
+//   border: "1in",
+// }
 
-const html = fs.readFileSync(source).toString()
+// const html = fs.readFileSync(source).toString()
 
-// Massive hack to create a server here because reading CSS files from the
-// filesystem with the base option does not work with root-relative URLs.
-const server = httpServer.createServer({root: pathPrefix})
-server.listen(PORT)
+// // Massive hack to create a server here because reading CSS files from the
+// // filesystem with the base option does not work with root-relative URLs.
+// const server = httpServer.createServer({root: pathPrefix})
+// server.listen(PORT)
 
-pdf.create(html, options).toFile(target, (err, res) => {
-  server.close()
-  console.log("PDF generated", err, res)
+// pdf.create(html, options).toFile(target, (err, res) => {
+//   server.close()
+//   console.log("PDF generated", err, res)
+// })
+
+const markdownpdf = require("markdown-pdf")
+
+const options = {cssPath}
+
+markdownpdf(options).from(source).to(target, () => {
+  console.log("Done")
 })
